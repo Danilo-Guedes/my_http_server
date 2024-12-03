a08082d8ddea6726d022d275ce518b701353812f
Danilo Paz Guedes de Freitas <danilotiba@gmail.com>
2024-12-02 23:47:18 -0300

    changing Request properties to use &str instead of String to avoid unnecessary heap allocation and dealing with lifetimes

--- src/http/request.rs
+++ src/http/request.rs
@@ -5,23 +5,23 @@ use std::error::Error;
 use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
 use std::str::Utf8Error;
 
-pub struct Request {
-    path: String,
-    query_string: Option<String>,
+pub struct Request<'buf> {
+    path: &'buf str,
+    query_string: Option<&'buf str>,
     method: Method,
 }
 
-impl Request {
+impl<'buf> Request<'buf> {
     fn from_byte_array(buf: &[u8]) {
         unimplemented!();
     }
 }
 
-impl TryFrom<&[u8]> for Request {
+impl<'buf> TryFrom<&'buf [u8]> for Request<'buf> {
     type Error = ParseError;
 
     // GET /search?name=abc&sort=1 HTTP/1.1
-    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+    fn try_from(buf: &'buf [u8]) -> Result<Self, Self::Error> {
         let request = str::from_utf8(buf)?;
 
         let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
@@ -41,13 +41,15 @@ impl TryFrom<&[u8]> for Request {
             path = &path[..i];
         }
 
-        unimplemented!()
+        Ok(Self {
+            path,
+            query_string,
+            method,
+        })
     }
 }
 
 fn get_next_word(request: &str) -> Option<(&str, &str)> {
-    let mut iter = request.chars();
-
     for (i, c) in request.chars().enumerate() {
         if c == ' ' || c == '\r' {
             // if it founds a space or a carriage return it will return the word and the rest of the string
