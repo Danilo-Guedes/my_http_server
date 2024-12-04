dd6fff16d5bddc2216e7c1859eed5439d7e4f731
Danilo Paz Guedes de Freitas <danilotiba@gmail.com>
2024-12-03 23:05:16 -0300

    add a public dir and retrieve in the main fn using envs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use server::Server;
+use std::env;
 use website_handler::WebsiteHandler;
-
 
 mod http;
 mod server;
@@ -8,6 +8,8 @@ mod website_handler;
 
 fn main() {
     let server = Server::new("127.0.0.1:8080".to_string());
+    let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
+    let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
 
-    server.run(WebsiteHandler);
+    server.run(WebsiteHandler::new(public_path));
 }
